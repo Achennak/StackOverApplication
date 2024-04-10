f418aaf9379626cdfc0d5aa339bb101579eacf1e
Rohith Janardhan <rohithjanardhan98@gmail.com>
2024-04-10 19:09:12 -0400

    test fix

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -34,6 +34,7 @@ const LoginPage = () => {
             value={email}
             onChange={(e) => setEmail(e.target.value)}
             className="w-full px-4 py-2 border border-gray-300 rounded-md"
+            data-testId="email-input-field"
             required
           />
         </div>
@@ -47,6 +48,7 @@ const LoginPage = () => {
             value={password}
             onChange={(e) => setPassword(e.target.value)}
             className="w-full px-4 py-2 border border-gray-300 rounded-md"
+            data-testId="password-input-field"
             required
           />
         </div>
