bf7a74d30251c1bd5b14a2d34f9a49dc68093bbb
Rohith Janardhan <rohithjanardhan98@gmail.com>
2024-04-20 21:59:27 -0400

    view Question e2e feature tests

--- client/src/components/sidebar.js
+++ client/src/components/sidebar.js
@@ -1,16 +1,28 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const Sidebar = () => {
+  const location = useLocation();
+
   return (
     <aside className="bg-gray-200 w-64 p-4">
       <ul>
         <li>
-          <Link to="/" className="block py-2 px-4 hover:bg-gray-300">
+          <Link
+            to="/"
+            className={`block py-2 px-4  ${
+              location.pathname === "/" ? "bg-blue-500 text-white" : ""
+            }`}
+          >
             Questions
           </Link>
         </li>
         <li>
-          <Link to="/tags" className="block py-2 px-4 hover:bg-gray-300">
+          <Link
+            to="/tags"
+            className={`block py-2 px-4  ${
+              location.pathname === "/tags" ? "bg-blue-500 text-white" : ""
+            }`}
+          >
             Tags
           </Link>
         </li>
